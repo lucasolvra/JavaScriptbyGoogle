18369db7fce75ccee292fa49b888c14a647cd634
lucasolvra <238343827+lucasolvra@users.noreply.github.com>
2025-10-30 17:58:11 -0300

    Kipper

---
+++ aula017/002.js
@@ -0,0 +1,20 @@
+let idades = [18, 25, 32, 11, 9, 45]
+
+let somaDasIdades = 0;
+for(let indice = 0; indice < somaDasIdades.length; indice++) {
+    let idade = idades[indice];
+    somaDasIdades = somarIdades(somaDasIdades, idade);
+}
+
+console.log("Soma das idades: " somaDasIdades);
+
+function verificaSeEMaiorDeIdade(idade){
+    return idade >= 18;
+}
+
+function somarIdades(somatorio, idade) {
+    let nossoTesteEscopo = "variável declarada dentro do if";
+    return somatorio + idade;
+}
+
+console.log(nossoTesteEscopo)
