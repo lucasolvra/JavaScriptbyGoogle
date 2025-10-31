4f050fb0c5ae2c3c8831da0994c0310630449d2b
Lucas Olvra <lucasolvradev@gmail.com>
2025-10-31 02:41:57 -0300

    Kipper02

--- aula017/002.js
+++ aula017/002.js
@@ -1,12 +1,14 @@
 let idades = [18, 25, 32, 11, 9, 45]
 
+var idade = idades[0];
+
 let somaDasIdades = 0;
 for(let indice = 0; indice < somaDasIdades.length; indice++) {
     let idade = idades[indice];
     somaDasIdades = somarIdades(somaDasIdades, idade);
 }
 
-console.log("Soma das idades: " somaDasIdades);
+console.log("Soma das idades: ", somaDasIdades);
 
 function verificaSeEMaiorDeIdade(idade){
     return idade >= 18;
