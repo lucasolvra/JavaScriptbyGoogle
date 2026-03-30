c3905378bbea5f493f03db89855deec5102eed1f
lucasolvra <238343827+lucasolvra@users.noreply.github.com>
2026-03-30 18:41:30 -0300

    todo-list-02

--- javascript-suprasupledev/ex011/scripts/11-todo-list2.js
+++ javascript-suprasupledev/ex011/scripts/11-todo-list2.js
@@ -1,11 +1,52 @@
-const todoList = [];
+const todoList = [{
+  name: 'make dinner',
+  dueDate: '28-09-1992'
+}, {
+  name: 'watch youtube',
+  dueDate: '28-09-1992'
+}];
+
+renderTodoList()
+
+function renderTodoList() {
+
+  let todoListHTML = '';
+
+  // LOOP THRU AN ARRAY
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    // TAKE NAME AND DUEDATE OUT OF "TODOLIST" OBJECT
+    const { name, dueDate } = todoObject; //DESTRUCTING
+    // GENERATING HTML
+    const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="delete-todo-button">Delete</button>
+    `;
+    todoListHTML += html;
+  }
+
+
+  document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
+
+}
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
+  const dateInputElement = document.querySelector('.js-due-date-input');
   const name = inputElement.value;
+  const dueDate = dateInputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  todoList.push({ //SHORTHAND PROPERTY
+    name,
+    dueDate
+});
 
   inputElement.value = '';
+
+  renderTodoList();
 }
