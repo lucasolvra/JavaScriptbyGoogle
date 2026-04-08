d153c8c3d6019b2505a17dacfe7633ec1fb08f07
Lucas Olvra <lucasolvradev@gmail.com>
2026-04-07 21:24:21 -0300

    lessons-todo

---
+++ javascript-suprasupledev/ex011/scripts/11-todo-list3.js
@@ -0,0 +1,31 @@
+const todoList = [];
+
+displayList();
+
+function displayList() {
+  
+  let todoListHTML = '';
+  
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+    
+  }
+  document.querySelector('.js-display-list')
+  .innerHTML = todoListHTML;
+}
+
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  todoList.push(name);
+
+  console.log(todoList);
+
+  inputElement.value = '';
+
+  displayList();
+}
